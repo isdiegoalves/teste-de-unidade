558b308a83890375984fd8c0914b414ffdfbb4ea
Diego Alves Oliveira da Silva <isdiegoalves@gmail.com>
2017-01-27 00:11:02 -0200

    Novo teste de sistema para cadastro e deleção de usuários

--- src/test/java/br/diego/leilaoonline/usuario/selenium/UsuariosSystemTest.java
+++ src/test/java/br/diego/leilaoonline/usuario/selenium/UsuariosSystemTest.java
@@ -8,6 +8,8 @@ import static org.hamcrest.Matchers.is;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import br.diego.leilaoonline.usuario.selenium.pages.UsuariosPage;
@@ -16,13 +18,13 @@ public class UsuariosSystemTest {
 
 	private WebDriver driver;
 	private UsuariosPage usuarios;
-	
+
 	@Before
 	public void before() {
 		driver = chromeWebDriver();
 		usuarios = new UsuariosPage(driver);
 	}
-	
+
 	@After
 	public void after() {
 		driver.quit();
@@ -33,37 +35,64 @@ public class UsuariosSystemTest {
 	public void deveCadastrarUsuario() {
 		usuarios.visita();
 		usuarios.novo().cadastra("Diego Alves", "diego@alves.com");
-		
+
 		assertThat(usuarios.existeNaListagem("Diego Alves", "diego@alves.com"), is(true));
 	}
-	
-	
+
+
 	@Test
 	public void naoDeveCadastrarUsuarioSemNome() {
 		usuarios.visita();
 		usuarios.novo().cadastra("", "diego@alves.com");
-		
+
 		assertThat(driver.getPageSource(), containsString("Nome obrigatorio!"));
 		assertThat(driver.getPageSource(), containsString("Salvar!"));
 	}
-	
+
 	@Test
 	public void naoDeveCadastrarUsuarioSemEmail() {
 		usuarios.visita();
 		usuarios.novo().cadastra("Diego Alves", "");
-		
+
 		assertThat(driver.getPageSource(), containsString("E-mail obrigatorio!"));
 		assertThat(driver.getPageSource(), containsString("Salvar!"));
 	}
-	
+
 	@Test
 	public void naoDeveCadastrarUsuarioSemEmailESemEmail() {
 		usuarios.visita();
 		usuarios.novo().cadastra("", "");
-		
+
 		assertThat(driver.getPageSource(), containsString("Nome obrigatorio!"));
 		assertThat(driver.getPageSource(), containsString("E-mail obrigatorio!"));
 		assertThat(driver.getPageSource(), containsString("Salvar!"));
 
 	}
+
+	@Test
+	public void deveCadastrarEExcluirUsuario() {
+
+		usuarios.visita();
+		usuarios.novo().cadastra("Diego Alves Oliveira da Silva", "isdiegoalves@gmail.com");
+
+		assertThat(usuarios.existeNaListagem("Diego Alves Oliveira da Silva", "isdiegoalves@gmail.com"), is(true));
+
+		int posicao = 1; // queremos o 1o botao da pagina
+		driver.findElements(By.tagName("button")).get(posicao-1).click();
+		// pega o alert que está aberto
+		Alert alert = driver.switchTo().alert();
+		// confirma
+		alert.accept();
+	}
+	
+	@Test
+    public void deveDeletarUmUsuario() {
+		usuarios.visita();
+        usuarios.novo().cadastra("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br");
+        assertThat(usuarios.existeNaListagem ("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"), is(true));
+
+        usuarios.deletaUsuarioNaPosicao(1);
+
+        assertThat(usuarios.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"), is(false));
+    }
 }
